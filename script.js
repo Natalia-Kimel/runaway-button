ac9ef07c356751e404df1c13720ea7cb953725d4
Natalia Kimel <nata@rus9n.com>
2024-11-14 19:03:37 +0300

    created

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const noButton = document.querySelector(".no-btn");
+    const yesButton = document.querySelector(".yes-btn");
+    const img = document.querySelector(".popup-img");
+  
+    function moveButtonRandomly() {
+      const windowWidth = window.innerWidth;
+      const windowHeight = window.innerHeight;
+  
+      // Generate random positions within the screen boundaries
+      const randomX = Math.random() * (windowWidth - noButton.offsetWidth);
+      const randomY = Math.random() * (windowHeight - noButton.offsetHeight);
+  
+      // Apply the random positions
+      noButton.style.left = `${randomX}px`;
+      noButton.style.top = `${randomY}px`;
+    }
+  
+    noButton.addEventListener("mouseover", moveButtonRandomly);
+    yesButton.addEventListener("click", () => {
+        img.style.display = "block";
+    });
+});  
